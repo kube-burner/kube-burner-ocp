88ddf964b71656e15e29dc7b87f1ec4bf654d138
Joe Talerico <rook@redhat.com>
2024-07-24 15:06:58 -0400

    Updating node-density
    Based on feedback, updating node-density based on feedback

--- node-density.go
+++ node-density.go
@@ -32,6 +32,10 @@ func NewNodeDensity(wh *workloads.WorkloadHelper) *cobra.Command {
 	var containerImage string
 	var metricsProfiles []string
 	var rc int
+	var iterationsPerNamespace, podsPerNode, churnCycles, churnPercent int
+	var podReadyThreshold, churnDuration, churnDelay time.Duration
+	var containerImage, churnDeletionStrategy string
+	var namespacedIterations, churn bool
 	cmd := &cobra.Command{
 		Use:          "node-density",
 		Short:        "Runs node-density workload",
@@ -61,9 +65,19 @@ func NewNodeDensity(wh *workloads.WorkloadHelper) *cobra.Command {
 			os.Exit(rc)
 		},
 	}
+	}
+
+	cmd.Flags().BoolVar(&churn, "churn", false, "Enable churning")
+	cmd.Flags().IntVar(&churnCycles, "churn-cycles", 0, "Churn cycles to execute")
+	cmd.Flags().DurationVar(&churnDuration, "churn-duration", 1*time.Hour, "Churn duration")
+	cmd.Flags().DurationVar(&churnDelay, "churn-delay", 2*time.Minute, "Time to wait between each churn")
+	cmd.Flags().StringVar(&churnDeletionStrategy, "churn-deletion-strategy", "gvr", "Churn deletion strategy to use")
+	cmd.Flags().IntVar(&churnPercent, "churn-percent", 10, "Percentage of job iterations that kube-burner will churn each round")
 	cmd.Flags().IntVar(&podsPerNode, "pods-per-node", 245, "Pods per node")
 	cmd.Flags().DurationVar(&podReadyThreshold, "pod-ready-threshold", 15*time.Second, "Pod ready timeout threshold")
 	cmd.Flags().StringVar(&containerImage, "container-image", "gcr.io/google_containers/pause:3.1", "Container image")
 	cmd.Flags().StringSliceVar(&metricsProfiles, "metrics-profile", []string{"metrics.yml"}, "Comma separated list of metrics profiles to use")
+	cmd.Flags().BoolVar(&namespacedIterations, "namespaced-iterations", false, "Namespaced iterations")
+	cmd.Flags().IntVar(&iterationsPerNamespace, "iterations-per-namespace", 1000, "Iterations per namespace")
 	return cmd
 }
