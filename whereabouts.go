485f0e14d3e68feda3b1c06c4ee8ea521539efc4
Joe Talerico aka rook <joe.talerico@gmail.com>
2024-11-20 06:35:53 -0500

    Address linting
    Signed-off-by: Joe Talerico aka rook <joe.talerico@gmail.com>

--- whereabouts.go
+++ whereabouts.go
@@ -15,45 +15,45 @@
 package ocp
 
 import (
-        "fmt"
-        "os"
-        "time"
+	"fmt"
+	"os"
+	"time"
 
-        "github.com/kube-burner/kube-burner/pkg/workloads"
+	"github.com/kube-burner/kube-burner/pkg/workloads"
 
-        "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 // NewWhereabouts
 func NewWhereabouts(wh *workloads.WorkloadHelper) *cobra.Command {
-        var iterations int
+	var iterations int
 	var fast bool
-        var podReadyThreshold time.Duration
-        var containerImage string
-        var metricsProfiles []string
-        var rc int
-        cmd := &cobra.Command{
-                Use:          "whereabouts",
-                Short:        "Runs whereabouts workload",
-                SilenceUsage: true,
-                PreRun: func(cmd *cobra.Command, args []string) {
-                        os.Setenv("JOB_ITERATIONS", fmt.Sprint(iterations))
-                        os.Setenv("POD_READY_THRESHOLD", fmt.Sprintf("%v", podReadyThreshold))
-                        os.Setenv("CONTAINER_IMAGE", containerImage)
+	var podReadyThreshold time.Duration
+	var containerImage string
+	var metricsProfiles []string
+	var rc int
+	cmd := &cobra.Command{
+		Use:          "whereabouts",
+		Short:        "Runs whereabouts workload",
+		SilenceUsage: true,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			os.Setenv("JOB_ITERATIONS", fmt.Sprint(iterations))
+			os.Setenv("POD_READY_THRESHOLD", fmt.Sprintf("%v", podReadyThreshold))
+			os.Setenv("CONTAINER_IMAGE", containerImage)
 			os.Setenv("FAST", fmt.Sprint(fast))
-                },
-                Run: func(cmd *cobra.Command, args []string) {
-                        setMetrics(cmd, metricsProfiles)
-                        rc = wh.Run(cmd.Name())
-                },
-                PostRun: func(cmd *cobra.Command, args []string) {
-                        os.Exit(rc)
-                },
-        }
-        cmd.Flags().IntVar(&iterations, "iterations", 10, "Number of iterations - each iteration is 1 ns and 6 pods")
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			setMetrics(cmd, metricsProfiles)
+			rc = wh.Run(cmd.Name())
+		},
+		PostRun: func(cmd *cobra.Command, args []string) {
+			os.Exit(rc)
+		},
+	}
+	cmd.Flags().IntVar(&iterations, "iterations", 10, "Number of iterations - each iteration is 1 ns and 6 pods")
 	cmd.Flags().BoolVar(&fast, "fast", false, "Use Fast IPAM")
-        cmd.Flags().DurationVar(&podReadyThreshold, "pod-ready-threshold", 15*time.Second, "Pod ready timeout threshold")
-        cmd.Flags().StringVar(&containerImage, "container-image", "gcr.io/google_containers/pause:3.1", "Container image")
+	cmd.Flags().DurationVar(&podReadyThreshold, "pod-ready-threshold", 15*time.Second, "Pod ready timeout threshold")
+	cmd.Flags().StringVar(&containerImage, "container-image", "gcr.io/google_containers/pause:3.1", "Container image")
 	cmd.Flags().StringSliceVar(&metricsProfiles, "metrics-profile", []string{"metrics-aggregated.yml"}, "Comma separated list of metrics profiles to use")
-        return cmd
+	return cmd
 }
