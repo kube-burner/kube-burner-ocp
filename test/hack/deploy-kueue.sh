ad958d56ddbc129b6d6c6ac82765a7563451b69b
Raul Sevilla <rsevilla@redhat.com>
2025-09-05 12:00:14 +0200

    Update hack
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- test/hack/deploy-kueue.sh
+++ test/hack/deploy-kueue.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 oc apply -f kueue/manifests.yml
+echo "Waiting 10s for kueue CRD to be created"
+sleep 10s
 oc apply -f kueue/kueue.yml
 echo "Waiting for Kueue Controller Manager..."
 timeout --foreground 300s bash -c 'until oc rollout status deployment kueue-controller-manager -n openshift-kueue-operator; do sleep 10; echo "Still waiting..."; done'
