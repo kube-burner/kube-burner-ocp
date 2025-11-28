feeba2e8ce333cf69862208df78ec6a916096b14
Raúl Sevilla <rsevilla@redhat.com>
2025-11-28 10:17:15 +0100

    Reorganize repository (#337)
    * Reorganize repository
    
    Move workloads to their own folder
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Rename commmon to clusterhealth
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    * Rename import
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>
    
    ---------
    
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/workloads/whereabouts.go
+++ pkg/workloads/whereabouts.go
@@ -12,36 +12,45 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ocp
+package workloads
 
 import (
 	"os"
+	"time"
 
 	"github.com/kube-burner/kube-burner/pkg/workloads"
+
 	"github.com/spf13/cobra"
 )
 
-// NewCrdScale holds the crd-scale workload
-func NewCrdScale(wh *workloads.WorkloadHelper) *cobra.Command {
+// NewWhereabouts
+func NewWhereabouts(wh *workloads.WorkloadHelper) *cobra.Command {
 	var iterations int
+	var fast bool
+	var podReadyThreshold time.Duration
+	var containerImage string
 	var metricsProfiles []string
 	var rc int
 	cmd := &cobra.Command{
-		Use:          "crd-scale",
-		Short:        "Runs crd-scale workload",
+		Use:          "whereabouts",
+		Short:        "Runs whereabouts workload",
 		SilenceUsage: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			setMetrics(cmd, metricsProfiles)
 			AdditionalVars["JOB_ITERATIONS"] = iterations
-
+			AdditionalVars["POD_READY_THRESHOLD"] = podReadyThreshold
+			AdditionalVars["CONTAINER_IMAGE"] = containerImage
+			AdditionalVars["FAST"] = fast
+			setMetrics(cmd, metricsProfiles)
 			rc = wh.RunWithAdditionalVars(cmd.Name()+".yml", AdditionalVars, nil)
 		},
 		PostRun: func(cmd *cobra.Command, args []string) {
 			os.Exit(rc)
 		},
 	}
-	cmd.Flags().IntVar(&iterations, "iterations", 0, "Number of CRDs to create")
+	cmd.Flags().IntVar(&iterations, "iterations", 10, "Number of iterations - each iteration is 1 ns and 6 pods")
+	cmd.Flags().BoolVar(&fast, "fast", false, "Use Fast IPAM")
+	cmd.Flags().DurationVar(&podReadyThreshold, "pod-ready-threshold", 15*time.Second, "Pod ready timeout threshold")
+	cmd.Flags().StringVar(&containerImage, "container-image", "gcr.io/google_containers/pause:3.1", "Container image")
 	cmd.Flags().StringSliceVar(&metricsProfiles, "metrics-profile", []string{"metrics-aggregated.yml"}, "Comma separated list of metrics profiles to use")
-	cmd.MarkFlagRequired("iterations")
 	return cmd
 }
