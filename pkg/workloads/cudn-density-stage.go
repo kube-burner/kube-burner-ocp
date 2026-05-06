d7a222d846c56c9bb7b5f04ab10cfbafeb9baf08
venkataanil <vkommadi@redhat.com>
2026-05-06 14:27:16 +0530

    Run udn-density in stages
    This PR allows to test https://github.com/kube-burner/kube-burner/pull/1236
    This allows single job for udn-density and cudn-density testing.
    
    Signed-off-by: venkataanil <vkommadi@redhat.com>

---
+++ pkg/workloads/cudn-density-stage.go
@@ -0,0 +1,92 @@
+// Copyright 2025 The Kube-burner Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package workloads
+
+import (
+	"os"
+	"time"
+
+	"github.com/kube-burner/kube-burner/v2/pkg/config"
+	"github.com/kube-burner/kube-burner/v2/pkg/workloads"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// NewCudnDensity holds cudn-density workload
+func NewCudnDensityStage(wh *workloads.WorkloadHelper) *cobra.Command {
+	var churnPercent, churnCycles, iterations, namespacesPerCudn int
+	var l3, pprof bool
+	var churnDelay, churnDuration, podReadyThreshold, pprofInterval, jobPause time.Duration
+	var churnMode string
+	var metricsProfiles []string
+	var rc int
+	cmd := &cobra.Command{
+		Use:          "cudn-density-stage",
+		Short:        "Runs cudn-density workload with tiered cross-namespace communication",
+		SilenceUsage: true,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			if iterations%namespacesPerCudn != 0 {
+				log.Fatalf("iterations (%d) must be divisible by namespaces-per-cudn (%d)", iterations, namespacesPerCudn)
+			}
+			if churnMode == string(config.ChurnNamespaces) && (churnDuration > 0 || churnCycles > 0) {
+				log.Fatal("churn-mode=namespaces is not supported for cudn-density: CUDN finalizers block namespace deletion. Use --churn-mode=objects instead")
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			setMetrics(cmd, metricsProfiles)
+			if l3 {
+				log.Info("Layer 3 topology enabled")
+			} else {
+				log.Info("Layer 2 topology enabled")
+			}
+			if churnDuration > 0 || churnCycles > 0 {
+				log.Info("Churn is enabled")
+			}
+
+			AdditionalVars["PPROF"] = pprof
+			AdditionalVars["PPROF_INTERVAL"] = pprofInterval.String()
+			AdditionalVars["JOB_PAUSE"] = jobPause
+			AdditionalVars["CHURN_CYCLES"] = churnCycles
+			AdditionalVars["CHURN_DURATION"] = churnDuration
+			AdditionalVars["CHURN_DELAY"] = churnDelay
+			AdditionalVars["CHURN_PERCENT"] = churnPercent
+			AdditionalVars["CHURN_MODE"] = churnMode
+			AdditionalVars["JOB_ITERATIONS"] = iterations
+			AdditionalVars["NAMESPACES_PER_CUDN"] = namespacesPerCudn
+			AdditionalVars["POD_READY_THRESHOLD"] = podReadyThreshold
+			AdditionalVars["ENABLE_LAYER_3"] = l3
+			wh.SetMeasurements(cudnMeasurementFactoryMap)
+			rc = RunWorkload(cmd, wh, cmd.Name()+".yml")
+		},
+		PostRun: func(cmd *cobra.Command, args []string) {
+			os.Exit(rc)
+		},
+	}
+	cmd.Flags().BoolVar(&l3, "layer3", false, "Use Layer3 topology instead of Layer2")
+	cmd.Flags().BoolVar(&pprof, "pprof", false, "Enable pprof collection for ovnkube components")
+	cmd.Flags().DurationVar(&pprofInterval, "pprof-interval", 0, "Interval between pprof collections")
+	cmd.Flags().DurationVar(&jobPause, "job-pause", 1*time.Minute, "Pause after CUDN creation to allow OVN-K network settling before workload deployment")
+	cmd.Flags().IntVar(&churnCycles, "churn-cycles", 0, "Churn cycles to execute")
+	cmd.Flags().DurationVar(&churnDuration, "churn-duration", 0, "Churn duration")
+	cmd.Flags().DurationVar(&churnDelay, "churn-delay", 2*time.Minute, "Time to wait between each churn")
+	cmd.Flags().IntVar(&churnPercent, "churn-percent", 10, "Percentage of job iterations that kube-burner will churn each round")
+	cmd.Flags().StringVar(&churnMode, "churn-mode", string(config.ChurnObjects), "Churn mode: objects (namespaces mode is not supported due to CUDN finalizer constraints)")
+	cmd.Flags().IntVar(&iterations, "iterations", 0, "Total number of namespaces to create")
+	cmd.Flags().IntVar(&namespacesPerCudn, "namespaces-per-cudn", 5, "Number of namespaces sharing the same CUDN")
+	cmd.Flags().DurationVar(&podReadyThreshold, "pod-ready-threshold", 2*time.Minute, "Pod ready timeout threshold")
+	cmd.Flags().StringSliceVar(&metricsProfiles, "metrics-profile", []string{"metrics.yml"}, "Comma separated list of metrics profiles to use")
+	cmd.MarkFlagRequired("iterations")
+	return cmd
+}
